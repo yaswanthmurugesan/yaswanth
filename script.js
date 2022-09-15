150b79ea676e5c271083c140b0338d5448f1b51f
yaswanthmurugesan <113608421+yaswanthmurugesan@users.noreply.github.com>
2022-09-15 18:57:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "papa" && b=="19-09-2002")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
