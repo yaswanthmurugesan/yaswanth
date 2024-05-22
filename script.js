a99ddd3081f5cb41f80bddf2d7f27cefc139aea6
yaswanthmurugesan <113608421+yaswanthmurugesan@users.noreply.github.com>
2024-05-22 09:41:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var a=prompt("Enter UserName");
  var b=prompt("Enter password");
-  	if(a== "papa" && b=="29-05-2024")
+  	if(a== "papa" && b=="29/05/2024")
   	{
   	    window.location.href="start.html";
   	}
